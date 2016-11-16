e46c67778d6a593ba95a8f2c0eda3968f453be80
Admiral Potato <admiral@nuclearpixel.com>
2016-11-15 23:44:58 -0700

    Adding a settings bar toggle - You wouldn't believe how much effort it took to get a hamburger

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,30 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		showSidebar: true
+		showSidebar: false
+	},
+	methods: {
+		settingsToggle: function(){
+			this.showSidebar = !this.showSidebar;
+			setTimeout(resizeWindowEventHandler,0);
+		}
 	},
 	template: `
 		<div class="h-100 container-fluid">
 			<div class="h-100 row">
-				<div class="h-100 col-xs-12 col-sm-9 noPad">
-					<div class="h-100 main">
-						<viewport />
-					</div>
+				<div class="settingsToggle"><a class="icon fa fa-bars" @click="settingsToggle"></a></div>
+				<div class="h-100 col-xs-12 main" :class="{'col-md-9': showSidebar}">
+					<viewport />
 				</div>
-				<div class="h-100 col-xs-12 col-sm-3 noPad">
-					<div class="h-100 sidebar">
+				<div class="h-100 col-xs-12 noPad sidebar" :class="{'col-md-3': showSidebar}" v-if="showSidebar">
+					<div class="h-100">
 						<div class="card">
 							<div class="card-header">Sidebar</div>
 							<div class="card-block">
 
 								<h4 class="card-title">Settings Title</h4>
 								<p class="card-text">hell yeah this is where settings live</p>
-
+								<hr />
 								<h4 class="card-title">Even More Settings Title</h4>
 								<p class="card-text">hell yeah this is where settings live</p>
 
