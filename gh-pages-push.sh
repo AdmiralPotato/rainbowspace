6950c76d5f4927389c0b7baa154dd64f5a9a7273
Admiral Potato <admiral@nuclearpixel.com>
2016-11-27 17:15:19 -0700

    Improving gh-page launch script

--- gh-pages-push.sh
+++ gh-pages-push.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-git subtree push --prefix public origin gh-pages
+read -p "Are you SURE you want to delete your remote gh-pages branch and replace it with a fresh one? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	git -c core.quotepath=false push --progress --porcelain origin :gh-pages
+	git subtree push --prefix public origin gh-pages
+fi
